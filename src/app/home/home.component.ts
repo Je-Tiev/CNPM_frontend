bb786eb384d0c103a205bb042dca1cec6da673d6
Je-Tiev <vistaroses141@gmail.com>
2024-11-08 11:13:13 +0700

    LoginPage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { SignupComponent } from '../signup/signup.component';
 import { ForgotPasswordComponent } from '../forgot-password/forgot-password.component';
+import { LoginComponent } from '../login/login.component';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -25,4 +26,12 @@ export class HomeComponent implements OnInit {
     dialogConfig.width = "550px";
     this.dialog.open(ForgotPasswordComponent, dialogConfig);
   }
+
+  handleLoginAction(){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = "550px";
+    this.dialog.open(LoginComponent, dialogConfig);
+  }
+
+  
 }
